41e64252f7ab45b2c5735d73660b7ab5d74c3da7
sidsgusv <95411237+sidsgusv@users.noreply.github.com>
2024-05-31 16:36:46 +0300

    UserController

--- src/main/java/com/example/pathfind/web/UserController.java
+++ src/main/java/com/example/pathfind/web/UserController.java
@@ -1,4 +1,22 @@
 package com.example.pathfind.web;
 
+import com.example.pathfind.web.dto.UserRegisterDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/users/register")
+    public String viewRegister(){
+        return "register";
+    }
+
+
+    @PostMapping("/users/register")
+    public String doRegister(UserRegisterDTO userRegisterDTO){
+        return "register";
+    }
+
 }
