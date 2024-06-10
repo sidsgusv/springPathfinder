e4c63821d219b86098bda2eba88015f55b94aaac
sidsgusv <95411237+sidsgusv@users.noreply.github.com>
2024-06-10 19:43:12 +0300

    Starting validation3

--- src/main/java/com/example/pathfind/web/UserController.java
+++ src/main/java/com/example/pathfind/web/UserController.java
@@ -7,19 +7,21 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
-    @GetMapping("/users/register")
+    @GetMapping("/register")
     public String viewRegister(Model model) {
         model.addAttribute("registerData",new UserRegisterDTO());
         return "register";
     }
 
 
-    @PostMapping("/users/register")
+    @PostMapping("/register")
     public String doRegister(
             @Valid UserRegisterDTO userRegisterDTO,
             BindingResult bindingResult,
@@ -36,7 +38,7 @@ public class UserController {
 
     }
 
-    @GetMapping("/users/login")
+    @GetMapping("/login")
     public String viewLogin(){
         return "login";
     }
